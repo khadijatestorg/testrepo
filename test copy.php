6b5d47436047661eb6de2964b29f8f22429db750
Khadija Hakim <khadija@ashlarglobal.com>
2025-01-01 19:19:38 +0500

    test commit

---
+++ test copy.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'apiKey' => 'OPENAI_API_KEY',
+    'model' => 'OPENAI_MODEL', 'gpt-3.5-turbo',
+    'trelloApiUrl' => 'trelloApiUrl',
+    'trelloWebHookUrl' => 'trelloWebHookUrl'
+];
